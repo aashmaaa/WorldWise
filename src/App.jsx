cb487287e9f2d12c6c3f4373c2c7278b1a1c293a
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-08-13 16:17:14 +0545

    Implementing the city list

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,49 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import "./index.css";
+import CityList from "./components/CityList";
+import { useEffect, useState } from "react";
+
+const BASE_URL = "http://localhost:9000";
 
 function App() {
+  const [cities, setCities] = useState({});
+  const [isloading, setIsLoading] = useState(false);
+
+  useEffect(function () {
+    async function fetchCities() {
+      try {
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      } catch {
+        alert("Error loading data!!!");
+      } finally {
+        setIsLoading(false);
+      }
+    }
+    fetchCities();
+  }, []);
+
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Homepage />} />
         <Route path="product" element={<Product />} />
         <Route path="login" element={<Login />} />
-        <Route path="app" element={<AppLayout />} />
+        <Route path="app" element={<AppLayout />}>
+          <Route
+            index
+            element={<CityList cities={cities} isloading={isloading} />}
+          />
+          <Route
+            path="cities"
+            element={<CityList cities={cities} isloading={isloading} />}
+          />
+          <Route path="countries" element={<p> List of countries</p>} />
+          <Route path="form" element={<p> Form</p>} />
+        </Route>
         <Route path="pricing" element={<Pricing />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
---
+++ src/components/Map.jsx
@@ -0,0 +1,11 @@
+import styles from "./Map.module.css";
+
+function Map() {
+  return (
+    <div>
+      <div className={styles.mapContainer}>Map</div>
+    </div>
+  );
+}
+
+export default Map;

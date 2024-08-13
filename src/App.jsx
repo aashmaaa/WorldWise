deb1f0da654018338635e34fe09bce83e106ac3d
aashmaaa <109813735+aashmaaa@users.noreply.github.com>
2024-08-13 16:36:24 +0545

    Implementing the Countries List

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Product from "./pages/Product";
 import Pricing from "./pages/Pricing";
@@ -7,13 +8,13 @@ import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
 import "./index.css";
 import CityList from "./components/CityList";
-import { useEffect, useState } from "react";
+import CountriesList from "./components/CountriesList";
 
 const BASE_URL = "http://localhost:9000";
 
 function App() {
   const [cities, setCities] = useState({});
-  const [isloading, setIsLoading] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(function () {
     async function fetchCities() {
@@ -39,13 +40,16 @@ function App() {
         <Route path="app" element={<AppLayout />}>
           <Route
             index
-            element={<CityList cities={cities} isloading={isloading} />}
+            element={<CityList cities={cities} isLoading={isLoading} />}
           />
           <Route
             path="cities"
-            element={<CityList cities={cities} isloading={isloading} />}
+            element={<CityList cities={cities} isLoading={isLoading} />}
           />
-          <Route path="countries" element={<p> List of countries</p>} />
+          <Route
+            path="countries"
+            element={<CountriesList cities={cities} isLoading={isLoading} />}
+          />
           <Route path="form" element={<p> Form</p>} />
         </Route>
         <Route path="pricing" element={<Pricing />} />
---
+++ src/components/CountriesList.jsx
@@ -0,0 +1,31 @@
+/* eslint-disable react/prop-types */
+
+import Spinner from "./Spinner";
+import styles from "./CountriesList.module.css";
+import CountryItem from "./CountryItem";
+import Message from "./Message";
+
+function CountriesList({ cities, isLoading }) {
+  if (isLoading) return <Spinner />;
+
+  if (!cities.length)
+    return (
+      <Message message="Add your first city by clicking a city on a map" />
+    );
+
+  const countries = cities.reduce((arr, city) => {
+    if (!arr.map((el) => el.country).includes(city.country))
+      return [...arr, { country: city.country, emoji: city.emoji }];
+    else return arr;
+  }, []);
+
+  return (
+    <ul className={styles.countriesList}>
+      {countries.map((country) => (
+        <CountryItem country={country} key={country.id} />
+      ))}
+    </ul>
+  );
+}
+
+export default CountriesList;
